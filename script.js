5d1e7253ea16934f8c8be87b0acd8c9a7cadd5c1
array-in-a-matrix <nova.neutrino@protonmail.com>
2021-06-09 13:50:12 -0400

    darkmode

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-let randomthemes = [
-  ["#ff8008", "#ffc837"],
-  ["#4A00E0", "#8E2DE2"],
-  ["#FF512F", "#F09819"],
-  ["#FF5F6D", "#FFC371"],
-  ["#FF416C", "#FF4B2B"],
-  ["#ee0979", "#ff6a00"],
-  ["#ec008c", "#fc6767"],
-  ["#f857a6", "#ff5858"],
-  ["#00c3ff", "#ffff1c"],
-  ["#e1eec3", "#f05053"],
-  ["#a8ff78", "#78ffd6"],
-  ["#B3FFAB", "#12FFF7"],
-  ["#AAFFA9", "#11FFBD"],
-  ["#5433FF", "#20BDFF"],
-  ["#00c6ff", "#0072ff"],
-  ["#4e54c8", "#8f94fb"],
-  ["#396afc", "#2948ff"],
-  ["#673AB7", "#512DA8"],
-  ["#4776E6", "#8E54E9"],
-  ["#DA22FF", "#9733EE"],
+let random_light_theme = [
+  // ["#ff8008", "#ffc837"], //orange
+  // ["#4A00E0", "#8E2DE2"], //purple
+  // ["#FF512F", "#F09819"], //orange
+  // ["#FF5F6D", "#FFC371"], //pink
+  // ["#FF416C", "#FF4B2B"], //pink-orange
+  // ["#ee0979", "#ff6a00"], //pink-orange
+  // ["#ec008c", "#fc6767"], //pink
+  // ["#f857a6", "#ff5858"], //pink
+  // ["#00c3ff", "#ffff1c"], //blue-lime
+  // ["#e1eec3", "#f05053"], // white-pink
+  // ["#5433FF", "#20BDFF"], //ugly blue
+  ["#00c6ff", "#0072ff"], //light blue
+  ["#4e54c8", "#8f94fb"], //purple
+  ["#396afc", "#2948ff"], // blue
+  // ["#673AB7", "#512DA8"], //dark purple
+  // ["#4776E6", "#8E54E9"], //purple
+  // ["#DA22FF", "#9733EE"], // violet
+  // ["#c31432", "#240b36"], // red
+  // ["#ad5389", "#3c1053"], // purple
+];
+let random_dark_theme = [
+  // ["#ff8008", "#ffc837"], //orange
+  // ["#4A00E0", "#8E2DE2"], //purple
+  // ["#FF512F", "#F09819"], //orange
+  // ["#FF5F6D", "#FFC371"], //pink
+  // ["#FF416C", "#FF4B2B"], //pink-orange
+  // ["#ee0979", "#ff6a00"], //pink-orange
+  // ["#ec008c", "#fc6767"], //pink
+  // ["#f857a6", "#ff5858"], //pink
+  // ["#00c3ff", "#ffff1c"], //blue-lime
+  // ["#e1eec3", "#f05053"], // white-pink
+  // ["#5433FF", "#20BDFF"], //ugly blue
+  // ["#00c6ff", "#0072ff"], //light blue
+  // ["#4e54c8", "#8f94fb"], //purple
+  // ["#396afc", "#2948ff"], // blue
+  // ["#673AB7", "#512DA8"], //dark purple
+  // ["#4776E6", "#8E54E9"], //purple
+  // ["#DA22FF", "#9733EE"], // violet
+  ["#c31432", "#240b36"], // red
+  ["#ad5389", "#3c1053"], // purple
 ];
 
 function rand(items) {
   return items[~~(items.length * Math.random())];
 }
 
-let randcolor = rand(randomthemes);
-document.documentElement.style.setProperty("--left", randcolor[0]);
-document.documentElement.style.setProperty("--right", randcolor[1]);
+
+let chosen_color = rand(random_light_theme);
+
+function assign_color(random_color){
+document.documentElement.style.setProperty("--left", random_color[0])
+document.documentElement.style.setProperty("--right", random_color[1])
+}
+assign_color(chosen_color)
+
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
