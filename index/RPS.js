5be31a1a518210fe810cd310b26ff762dca3d18d
array-in-a-matrix <nova.neutrino@protonmail.com>
2021-06-16 13:55:26 -0400

    Score keeping

--- index/RPS.js
+++ index/RPS.js
@@ -1,7 +1,7 @@
-let userChoice = ""
+let userChoice = "";
 
 const getUserChoice = (userInput) => {
-    userChoice = userInput
+  userChoice = userInput;
 };
 
 function getComputerChoice() {
@@ -17,52 +17,87 @@ function getComputerChoice() {
 
 function determineWinner(userChoice, computerChoice) {
   if (userChoice === computerChoice) {
-    return "Tie game!";
+    return "Tie";
   }
   if (userChoice === "rock") {
     if (computerChoice === "paper") {
-      return "Computer won!";
+      return "Computer";
     } else {
-      return "You won!";
+      return "Player";
     }
   }
   if (userChoice === "paper") {
     if (computerChoice === "scissors") {
-      return "Computer won!";
+      return "Computer";
     } else {
-      return "You won!";
+      return "Player";
     }
   }
   if (userChoice === "scissors") {
     if (computerChoice === "rock") {
-      return "Computer won!";
+      return "Computer";
     } else {
-      return "You won!";
+      return "Player";
     }
   }
-  if (userChoice === "bomb") {
-    return "You won with cheats!";
-  }
 }
+
+const score = {
+  player: 0,
+  cpu: 0,
+  ties: 0,
+  rounds: 0,
+};
 
 function playGame() {
   var computerChoice = getComputerChoice();
-  console.log("Player: " + userChoice);
-  console.log("CPU: " + computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
+  const winner = determineWinner(userChoice, computerChoice);
 
-  var div = document.getElementById('score')
+  score.rounds++;
+  switch (winner) {
+    case "Player":
+      score.player++;
+      break;
+    case "Computer":
+      score.cpu++;
+      break;
+    case "Tie":
+      score.ties++;
+      break;
+  }
+
+  console.log("\nPlayer: " + userChoice);
+  console.log("CPU: " + computerChoice);
+  if (winner != "Tie") {
+    console.log("Winner: " + winner);
+  } else {
+    console.log("Tie game!");
+  }
+  console.log(score);
+
+  var div = document.getElementById("choice");
   var p = document.getElementById("text");
   var strong = document.getElementById("winner");
+  var scorePlayer = document.getElementById("scorePlayer");
+  var scoreCPU = document.getElementById("scoreCPU");
+  var round = document.getElementById("round");
 
-  p.textContent = `You:     ${userChoice}`
-  p.textContent += ` CPU:    ${computerChoice}`
-  strong.textContent = determineWinner(userChoice, computerChoice);     
-  document.getElementById("game").appendChild(div).appendChild(p)
-  document.getElementById("game").appendChild(div).appendChild(strong)
+  scorePlayer.textContent = `Player wins: ${score.player}`;
+  scoreCPU.textContent = `Computer wins: ${score.cpu}`;
+  round.textContent = `Round ${score.rounds}`;
+
+  p.textContent = `You:     ${userChoice}`;
+  p.textContent += ` CPU:    ${computerChoice}`;
+  strong.textContent = winner;
+
+  document.getElementById("game").appendChild(div).appendChild(p);
+  document.getElementById("game").appendChild(div).appendChild(strong);
+  document.getElementById("gameScore").appendChild(scorePlayer);
+  document.getElementById("gameScore").appendChild(scoreCPU);
+  document.getElementById("gameScore").appendChild(round);
 }
 
-function play(input){
-    getUserChoice(input)
-    playGame()
+function play(input) {
+  getUserChoice(input);
+  playGame();
 }
