627d6acf4d0a5d104bc9967a25bff28cd7fdefd0
array-in-a-matrix <nova.neutrino@protonmail.com>
2021-07-16 20:22:59 -0400

    minor adjustments

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 let random_theme = [
-  // ["#ff8008", "#ffc837"], //orange
-  // ["#4A00E0", "#8E2DE2"], //purple
-  // ["#FF512F", "#F09819"], //orange
-  // ["#FF5F6D", "#FFC371"], //pink
-  // ["#FF416C", "#FF4B2B"], //pink-orange
-  // ["#ee0979", "#ff6a00"], //pink-orange
-  // ["#ec008c", "#fc6767"], //pink
-  // ["#f857a6", "#ff5858"], //pink
-  // ["#00c3ff", "#ffff1c"], //blue-lime
-  // ["#e1eec3", "#f05053"], // white-pink
-  // ["#5433FF", "#20BDFF"], //ugly blue
-  // ["#00c6ff", "#0072ff"], //light blue
-  // ["#4e54c8", "#8f94fb"], //purple
-  ["#396afc", "#2948ff"], //? blue
-  ["#673AB7", "#512DA8"], //dark purple
-  // ["#4776E6", "#8E54E9"], //purple
-  // ["#DA22FF", "#9733EE"], // violet
-  ["#c31432", "#240b36"], //? red
-  ["#ad5389", "#3c1053"], //? purple
+  ["#c31432", "#240b36"],
+  ["#ad5389", "#3c1053"],
+  ["#2e1c30", "#937898"],
 ];
 
 function rand(items) {
