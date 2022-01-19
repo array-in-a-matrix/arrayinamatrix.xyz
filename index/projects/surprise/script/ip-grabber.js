47f3a406d3e2cab151bcdea99c160041098a81cb
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-01-19 00:38:06 -0500

    appened to end of file

--- index/projects/surprise/script/ip-grabber.js
+++ index/projects/surprise/script/ip-grabber.js
@@ -1,7 +1,6 @@
 fs = require('fs');
 
 function getIP(json) {
-  const ip = json.ip
-  document.write("Your public IP address is: ", ip);
-  fs.appendFileSync('ip-addresses.log', ip + '\n', 'utf8');
+  document.write("Your public IP address is: ", json.ip);
+  fs.appendFileSync('ip-addresses.log', json.ip + '\n', 'utf8');
 }
