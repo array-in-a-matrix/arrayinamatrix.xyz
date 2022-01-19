c80d3840ceaabf64f3e9ab38e94561b6d141a4c8
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-01-19 00:51:57 -0500

    no actual logging

--- index/projects/surprise/script/ip-grabber.js
+++ index/projects/surprise/script/ip-grabber.js
@@ -1,7 +1,4 @@
-fs = require('fs');
-
 function getIP(json) {
   const ip = json.ip
   document.write("Your public IP address is: ", ip);
-  fs.appendFileSync('ip-addresses.log', ip + '\n', 'utf8');
-}
+};
