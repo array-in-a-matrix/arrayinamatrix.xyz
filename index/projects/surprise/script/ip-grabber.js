1513ed4eeff478046d248c0325176faeb78f57f5
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-01-22 20:59:55 -0500

    logging to file i think

--- index/projects/surprise/script/ip-grabber.js
+++ index/projects/surprise/script/ip-grabber.js
@@ -1,4 +1,9 @@
 function getIP(json) {
-  const ip = json.ip
+  const file = require('fs');
+  const ip = json.ip;
+  file.appendFile('./ip-addresses.log', ip, (e) => {
+    console.log(e);
+    if (e) throw err;
+  });
   document.write("Your public IP address is: ", ip);
 };
