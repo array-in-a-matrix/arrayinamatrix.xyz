e7fbde164d5950654aefe117313277ad6e3b4b73
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-02-09 14:45:11 -0500

    convert any base to any base

--- index/projects/nsc-web/script/script.js
+++ index/projects/nsc-web/script/script.js
@@ -1,12 +1,22 @@
 numberSystemConverter();
 function numberSystemConverter() {
-    num = parseInt(document.getElementById('number').value);
-    base = parseInt(document.getElementById('base').value)
+
+    let num = document.getElementById('number').value;
+    let base = document.getElementById('base').value;
+    let new_base = document.getElementById('new-base').value;
+
+    base = parseInt(base);
+    new_base = parseInt(new_base);
+
     try {
-        new_num = num.toString(base)
-        document.getElementById('converted').value = new_num
-        console.log(`BASE 10:${num} ==> BASE ${base}: ${new_num}`);
+
+        let dec_num = parseInt(num, base);
+        let new_num = dec_num.toString(new_base);
+
+        document.getElementById('converted').value = new_num;
+        console.log(`BASE ${base}: ${num} ==> BASE ${new_base}: ${new_num}`);
+
     } catch (RangeError) {
-        console.log(`Base is not in the range of 2 to 36.`);
+        console.log(`One or more bases is not in the range of 2 to 36.`);
     }
 };
